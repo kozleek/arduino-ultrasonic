9b00fa3eedc66ccaf871914ee39ed33593dd8a9f
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-05 11:34:34 +0200

    Arduino nano + OLED display.

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,15 +1,9 @@
 #include <OneWire.h>
 #include <Wire.h>
-#include <hd44780.h>
-#include <hd44780ioClass/hd44780_I2Cexp.h> // include i/o class header
+#include <SPI.h>
 #include <DallasTemperature.h>
-
-/**
- * Test Arduino UNO with ultrasonic sensor
- * Schema: https://wokwi.com/projects/395127928831719425
- */
-
-hd44780_I2Cexp lcd; // declare lcd object: auto locate & config display for hd44780 chip
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define PIN_ECHO 12
 #define PIN_TRIG 11
@@ -21,8 +15,24 @@ float distance = 0;
 OneWire oneWire(PIN_TEMP);           // Inicializace OneWire sběrnice na daném pinu
 DallasTemperature sensors(&oneWire); // Inicializace čidla DallasTemperature
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup()
 {
+    Serial.begin(9600);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+
     // initialize temperature sensor
     sensors.begin();
 
@@ -34,13 +44,20 @@ void setup()
     pinMode(PIN_LED, OUTPUT);
     digitalWrite(PIN_LED, LOW);
 
-    // initialize lcd screen
-    lcd.begin(16, 2);
-    lcd.backlight();
-    lcd.print("Ultrasonic v1.6");
-    delay(1000);
-    lcd.clear();
-    lcd.noBacklight();
+    display.clearDisplay();
+    display.setTextSize(1); // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0); // Start at top-left corner
+    display.cp437(true);     // Use full 256 char 'Code Page 437' font
+
+    display.println(F("Ultrasonic v2.1"));
+    display.println(F("\n"));
+    display.println(F("If you start me up"));
+    display.println(F("I'll never stop..."));
+
+    display.display();
+    delay(2000); // Pause for 2 seconds
+    display.clearDisplay();
 }
 
 void loop()
@@ -70,26 +87,27 @@ void loop()
         int temperature = sensors.getTempCByIndex(0); // Přečtení teploty z čidla
         // temperature = round(temperature * 10) / 10.0; // Zaokrouhlení na jedno desetinné místo
 
-        lcd.backlight();
-        lcd.setCursor(0, 0);
-        lcd.print(distance);
-        lcd.print("cm, ");
-        lcd.print(temperature);
-        lcd.print((char)223);
-        lcd.print("C");
+        display.clearDisplay();
+        display.setTextSize(1); // Normal 1:1 pixel scale
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(0, 0); // Start at top-left corner
+        display.cp437(true);     // Use full 256 char 'Code Page 437' font
 
-        lcd.setCursor(0, 1);
-        lcd.print("Days: ");
-        lcd.print(getDays(distance));
+        display.println(F("Ultrasonic v2.1"));
+        display.println(F("\n"));
+        display.println("Distance:    " + String(distance) + " cm");
+        display.println("Remaining:   " + String(getDays(distance)) + " day(s)");
+        display.println("\n");
+        display.println("Temperature: " + String(temperature) + " C");
 
-        delay(3000);
+        display.display();
+        delay(2000); // Pause for 2 seconds
     }
 
     else
     {
-        // Turn off the LCD backlight
-        lcd.noBacklight();
-        lcd.clear();
+        display.clearDisplay();
+        display.display();
     }
 }
 
@@ -97,6 +115,24 @@ int getDays(int distance)
 {
     // Calculate the number of days based on the distance
     int days = distance / 5;
+    String text = "";
+
+    if (days <= 0)
+    {
+        text = 'Full';
+    }
+    else if (days == 1)
+    {
+        text = String(days) + ' day';
+    }
+    else if (days > 1)
+    {
+        text = String(days) + ' days';
+    }
+    else
+    {
+        text = String(days);
+    }
 
     return days;
 }
